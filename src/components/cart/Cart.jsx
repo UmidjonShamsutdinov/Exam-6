fa2bf6893ab4d7c3d100fbea4637004e72a33604
UmidjonShamsutdinov <umid.2825@gmail.com>
2023-12-11 00:17:45 +0500

    last update

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,23 +3,27 @@ import "./Cart.scss"
 import { useSelector } from 'react-redux'
 import { Container } from '../../utils/Utils'
 import Card from '../card/Card'
+import Nav from '../../layout/nav/Nav'
 
 const Cart = () => {
     const {cart} = useSelector(state => state.add_to_cart_data)
   return (
-    <div className='cart__main'>
-        <Container>
-            <div className='carts'>
-                {
-                    cart.map(product=>
-                        product !== null && product !== undefined && <div className="cart">
-                            <Card product={product} key={product.id}/>
-                        </div>
-                        )
-                }
-            </div>
-        </Container>
-    </div>
+    <>
+        <Nav/>
+        <div className='cart__main'>
+            <Container>
+                <div className='carts'>
+                    {
+                        cart.map(product=>
+                            product !== null && product !== undefined && <div className="cart">
+                                <Card product={product} key={product.id}/>
+                            </div>
+                            )
+                    }
+                </div>
+            </Container>
+        </div>
+    </>
   )
 }
 
--- src/components/like/Like.jsx
+++ src/components/like/Like.jsx
@@ -3,22 +3,26 @@ import { useSelector } from 'react-redux'
 import Card from '../card/Card';
 import { Container } from '../../utils/Utils';
 import "./Like.scss"
+import Nav from '../../layout/nav/Nav';
 
 const Like = () => {
     let {like} = useSelector(state => state.liked_data)
     console.log(like);
   return (
-    <Container>
-        <div className='like_main'>
-            {
-                like.map(e=>
-                    <div className='like'>
-                        <Card product={e} key={e.id}/>
-                    </div>
-                    )
-            }
-        </div>
-    </Container>
+    <>
+        <Nav/>
+        <Container>
+            <div className='like_main'>
+                {
+                    like.map(e=>
+                        <div className='like'>
+                            <Card product={e} key={e.id}/>
+                        </div>
+                        )
+                }
+            </div>
+        </Container>
+    </>
   )
 }
 
